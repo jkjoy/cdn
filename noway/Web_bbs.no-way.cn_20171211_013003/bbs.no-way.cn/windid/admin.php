4287c427061b83edf2a05d22bb1aa82c163a5cf7
浪子 <jkjoy@live.cn>
2025-05-05 11:25:15 +0800

    del

--- noway/Web_bbs.no-way.cn_20171211_013003/bbs.no-way.cn/windid/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-error_reporting(E_ERROR | E_PARSE);
-
-require dirname(__DIR__).'/bootstrap.php';
-
-Wekit::run('windidadmin', array('router' => array()));
--- noway/Web_bbs.no-way.cn_20171211_013003/bbs.no-way.cn/windid/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-error_reporting(E_ERROR | E_PARSE);
-
-require dirname(__DIR__).'/bootstrap.php';
-
-$components = array('router' => array());
-Wekit::run('windid', $components);
